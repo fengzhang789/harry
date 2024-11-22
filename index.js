462819ee3d5814de0f69600d73a5808b7dd1e707
Feng Zhang <yunfengzhang789@gmail.com>
2024-11-22 15:55:35 -0500

    gaming

--- index.js
+++ index.js
@@ -1,18 +1,41 @@
-require('dotenv').config(); //initializes dotenv
-const Discord = require('discord.js'); //imports discord.js
+import { REST, Routes, Client, GatewayIntentBits } from 'discord.js';
+import dotenv from 'dotenv';
 
-const client = new Discord.Client(); //creates new client
+dotenv.config();
+
+const commands = [
+  {
+    name: 'ping',
+    description: 'Replies with Pong!',
+  },
+];
+
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+
+try {
+  console.log('Started refreshing application (/) commands.');
+
+  await rest.put(Routes.applicationCommands(process.env.CLIENT_ID, process.env.GUILD_ID), { body: commands });
+
+  console.log('Successfully reloaded application (/) commands.');
+} catch (error) {
+  console.error(error);
+}
+
+
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-//this line must be at the very end
-client.login(process.env.CLIENT_TOKEN); //signs the bot in with token
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isChatInputCommand()) return;
 
-
-client.on('message', msg => {
-  if (msg.content === 'gaming') {
-    msg.reply('gaming!');
+  if (interaction.commandName === 'ping') {
+    await interaction.reply('Pong!');
   }
 });
+
+client.login(process.env.TOKEN);
